c2edf9bf37861400afe886a67c3fcd7bd1ba4a62
t-quinn-t <qintutao@gmail.com>
2024-04-10 02:19:53 -0700

    fixed warnings

--- Makefile
+++ Makefile
@@ -1,4 +1,2 @@
 run: 
-	swipl -s middle_snake.pl middle_snake_test.pl
-diff: 
-	swipl -s middle_snake.pl mdiff.pl
+	swipl -s middle_snake.pl 
--- middle_snake.pl
+++ middle_snake.pl
@@ -67,7 +67,7 @@ mksnake_backward(Delta,Offset, K, D, FRArray, X) :-
   get_fr(Offset, KMinus, FRArray, XLeft),
   XLeft < XRight, % NOTE: if XRight == XLeft, we should still use XRight since we can move left 
   X is XLeft.
-mksnake_backward(Delta,Offset, K, _, FRArray, X) :-
+mksnake_backward(_,Offset, K, _, FRArray, X) :-
   % Moving Left from Right as there is no diaognal on the left
   KPlus #= K + 1,
   get_fr(Offset, KPlus, FRArray, XRight),
@@ -76,8 +76,8 @@ mksnake_backward(Delta,Offset, K, _, FRArray, X) :-
 % Searching forward and comparing two lists 
 % Returns the length of longest common prefix 
 search_forward([], [], 0) :- true.
-search_forward([], L, 0) :- true.
-search_forward(L, [], 0) :- true.
+search_forward([], _, 0) :- true.
+search_forward(_, [], 0) :- true.
 search_forward([A|Body0], [A|Body1], Offset) :- 
   Next #= Offset-1,
   search_forward(Body0, Body1, Next).
@@ -103,16 +103,16 @@ search_backward(A, B, Offset) :-
 % 
 % NOTE: in forward version, we compare from the NEXT string:
 %   keep searching while from[x+1+offset] == to[y+1+offset]
-extend_fr_forward(_, [], _, X, K, X, FRArray, FRArray) :- true.
-extend_fr_forward(_, _, [], X, K, X, FRArray, FRArray) :- true.
-extend_fr_forward(Offset, From, To, X, K, X, FRArrayIn, FRArrayOut) :-
+extend_fr_forward(_, [], _, X, _, X, FRArray, FRArray) :- true.
+extend_fr_forward(_, _, [], X, _, X, FRArray, FRArray) :- true.
+extend_fr_forward(Offset, _, To, X, K, X, FRArrayIn, FRArrayOut) :-
   diagonal_to_xy(K,X,Y),
   length(To, L),
   Y #> L-2, 
   Index #= Offset + K,
   append(H, [_|T], FRArrayIn), length(H, Index),
   append(H, [X|T], FRArrayOut).
-extend_fr_forward(Offset, From, To, X, K, X, FRArrayIn, FRArrayOut) :-
+extend_fr_forward(Offset, From, _, X, K, X, FRArrayIn, FRArrayOut) :-
   length(From, L),
   X #> L-2,
   Index #= Offset + K,
@@ -132,14 +132,14 @@ extend_fr_forward(Offset, From, To, X, K, XOut, FRArrayIn, FRArrayOut) :-
 
 % Good
 % Backward Version
-extend_fr_backward(_, [], _, X, K, X, FRArray, FRArray) :- true.
-extend_fr_backward(_, _, [], X, K, X, FRArray, FRArray) :- true.
-extend_fr_backward(_, [], [], X, K, X, FRArrayIn, FRArrayOut) :- 
+extend_fr_backward(_, [], _, X, _, X, FRArray, FRArray) :- true.
+extend_fr_backward(_, _, [], X, _, X, FRArray, FRArray) :- true.
+extend_fr_backward(Offset, [], [], X, K, X, FRArrayIn, FRArrayOut) :- 
   X #< 1,
   Index #= Offset + K,
   append(H, [_|T], FRArrayIn), length(H, Index),
   append(H, [X|T], FRArrayOut).
-extend_fr_backward(_, [], [], X, K, X, FRArrayIn, FRArrayOut) :- 
+extend_fr_backward(Offset, [], [], X, K, X, FRArrayIn, FRArrayOut) :- 
   diagonal_to_xy(K,X,Y),
   Y #< 1,
   Index #= Offset + K,
@@ -150,8 +150,8 @@ extend_fr_backward(Offset, From, To, X, K, XOut, FRArrayIn, FRArrayOut) :-
   diagonal_to_xy(K,X,Y),
   XX #= X+1,
   YY #= Y+1,
-  append(S0, L, From), length(S0, XX),
-  append(S1, R, To), length(S1, YY),
+  append(S0, _, From), length(S0, XX),
+  append(S1, _, To), length(S1, YY),
   search_backward(S0, S1, Move),
   (
     X-Move #< 0
@@ -170,11 +170,11 @@ extend_fr_backward(Offset, From, To, X, K, XOut, FRArrayIn, FRArrayOut) :-
 %    We check_finish_backward (return x,y if clash with forward searching)
 
 % Check Forward Search termination
-check_finish_forward(LX, LY, Offset, Delta, D, K, BackwardIn, X) :- 
+check_finish_forward(LX, LY, _, _, _, K, _, X) :- 
   Y #= X-K, 
   Y #= LY-1,
   X #= LX-1.
-check_finish_forward(LX, LY, Offset, Delta, D, K, BackwardIn, X) :- 
+check_finish_forward(_, _, Offset, Delta, D, K, BackwardIn, X) :- 
   % Delta is odd && k in [delta-d+1, delta+d-1]
   Delta mod 2 #= 1, 
   Delta + 1 - D < K + 1, 
@@ -183,7 +183,7 @@ check_finish_forward(LX, LY, Offset, Delta, D, K, BackwardIn, X) :-
   nth0(Index, BackwardIn, X).
 
 % Main K-Loop Iteration 
-kloop_iter_forward(Offset, From, To, Delta, D, K, Max, ForwardIn, ForwardIn, BackwardIn, -1, -1) :- 
+kloop_iter_forward(_, _, _, _, D, K, _, ForwardIn, ForwardIn, _, -1, -1) :- 
   K #> D. 
 kloop_iter_forward(Offset, From, To, Delta, D, K, Max, ForwardIn, ForwardOut, BackwardIn, XOut, YOut) :- 
   K #< D+1, 
@@ -207,14 +207,14 @@ check_finish_backward(Offset, Delta, D, K, ForwardIn, X) :-
   Index #= K + Offset, 
   nth0(Index, ForwardIn, X).
   
-kloop_iter_backward(Offset, From, To, Delta, D, K, Max, BackwardIn, BackwardIn, ForwardIn, -1, -1) :- 
+kloop_iter_backward(_, _, _, _, D, K, _, BackwardIn, BackwardIn, _, -1, -1) :- 
   K #> D. 
 kloop_iter_backward(Offset, From, To, Delta, D, K, Max, BackwardIn, BackwardOut, ForwardIn, XOut, YOut) :- 
   K #< D+1,
   Next #= K+2,
   KPlusDelta #= K + Delta,
   once(mksnake_backward(Delta, Offset, K, D, BackwardIn, X)), % X is FR on diagonal K in previous iteration
-  once(extend_fr_backward(Offset, From, To, X, KPlusDelta, XExt, BackwardIn, BackwardExt)),
+  once(extend_fr_backward(Offset, From, To, X, KPlusDelta, _, BackwardIn, BackwardExt)),
   (
      check_finish_backward(Offset, Delta, D, KPlusDelta, ForwardIn, X)
   -> XOut #= X, YOut #= X - K - Delta,
@@ -237,7 +237,7 @@ kloop_iter_backward(Offset, From, To, Delta, D, K, Max, BackwardIn, BackwardOut,
 % dloop_proc_2(From, To, Delta, D, Max, ForwardIn, BackwardExt, XOut,ForwardOut, BackwardOut) :- 
 %   dloop(From, To, Delta, D, Max, ForwardIn, BackwardIn, XOut, ForwardOut, BackwardOut).
 
-dloop(Offset, From, To, Delta, D, Max, ForwardIn, BackwardIn, -1, -1,ForwardOut, BackwardOut) :- 
+dloop(_, _, _, _, D, Max, _, _, -1, -1,_, _) :- 
   D #> div(Max+1, 2)+1.
 dloop(Offset, From, To, Delta, D, Max, ForwardIn, BackwardIn, XOut, YOut, ForwardOut, BackwardOut) :- 
   D #< div(Max+1, 2)+2, 
@@ -275,8 +275,6 @@ middle_snake(S0, S1, ReturnX, ReturnY) :-
   Max #= M + N,
   Offset #= (Max+1) // 2,
 
-  L #= Offset * 2 + 1, 
-  P #= Offset + 1,
   MMinus #= M-1,
 
   % Buffers for furthest reaching point 
@@ -330,56 +328,56 @@ test("Searching backwards produces correct movement (ab, ab) ") :-
   once(search_backward([a,b],[a,b], 2)).
 
 test('Getting the correct snake move (Forward, K=0):') :- 
-  mksnake_forward(3,0,0,[0,0,0,0,0,0], 0). 
+  once(mksnake_forward(3,0,0,[0,0,0,0,0,0], 0)). 
 test('Getting the correct snake move (Forward, K=-1, Left Boundary):') :- 
-  mksnake_forward(3,-1,1,[0,0,0,1,0,0], 1). 
+  once(mksnake_forward(3,-1,1,[0,0,0,1,0,0], 1)). 
 test('Getting the correct snake move (Forward, K=1, Right Boundary):') :- 
-  mksnake_forward(3,1,1,[0,0,0,1,0,0], 2). 
+  once(mksnake_forward(3,1,1,[0,0,0,1,0,0], 2)). 
 test('Getting the correct snake move (Forward, K=-2, Left Boundary User Right):') :- 
-  mksnake_forward(3,-2,2,[0,0,3,1,2,0], 3). 
+  once(mksnake_forward(3,-2,2,[0,0,3,1,2,0], 3)). 
 test('Getting the correct snake move (Forward, K=-2, Left Boundary Use Right):') :- 
-  mksnake_forward(3,-2,2,[0,0,1,1,2,0], 1). 
+  once(mksnake_forward(3,-2,2,[0,0,1,1,2,0], 1)). 
 test('Getting the correct snake move (Forward, K=0, Use Left):') :- 
-  mksnake_forward(3,0,2,[0,0,2,1,2,0], 3). 
+  once(mksnake_forward(3,0,2,[0,0,2,1,2,0], 3)). 
 test('Getting the correct snake move (Forward, K=0, Use Right):') :- 
-  mksnake_forward(3,0,2,[0,0,1,1,2,0], 2). 
+  once(mksnake_forward(3,0,2,[0,0,1,1,2,0], 2)). 
 test('Getting the correct snake move (Forward, K=2, Right Boundary User Left):') :- 
-  mksnake_forward(3,2,2,[0,0,1,1,2,0], 3). 
+  once(mksnake_forward(3,2,2,[0,0,1,1,2,0], 3)). 
 test('Getting the correct snake move (Forward, K=2, Right Boundary User Left):') :- 
-  mksnake_forward(3,2,2,[0,0,1,1,1,0], 2). 
+  once(mksnake_forward(3,2,2,[0,0,1,1,1,0], 2)). 
 
 test('Getting the correct snake move (Backward, Even Delta):') :- 
-  mksnake_backward(0, 3, 0, 0, [6,6,6,6,6,6], 6).
+  once(mksnake_backward(0, 3, 0, 0, [6,6,6,6,6,6], 6)).
 test('Getting the correct snake move (Backward, Even Delta, Moving Left):') :- 
-  mksnake_backward(0, 3, -1, 1, [6,6,6,5,6,6], 4).
+  once(mksnake_backward(0, 3, -1, 1, [6,6,6,5,6,6], 4)).
 test('Getting the correct snake move (Backward, Even Delta, Moving Up):') :- 
-  mksnake_backward(0, 3, 1, 1, [6,6,6,5,6,6], 5).
+  once(mksnake_backward(0, 3, 1, 1, [6,6,6,5,6,6], 5)).
 test('Getting the correct snake move (Backward, Even Delta, Taking Smaller One and Move Up):') :- 
-  mksnake_backward(0, 3, 0, 2, [6,6,4,5,6,6], 4).
+  once(mksnake_backward(0, 3, 0, 2, [6,6,4,5,6,6], 4)).
 test('Getting the correct snake move (Backward, Even Delta, Taking Smaller One and Move Left):') :- 
-  mksnake_backward(0, 3, 0, 2, [6,6,5,5,4,6], 3).
+  once(mksnake_backward(0, 3, 0, 2, [6,6,5,5,4,6], 3)).
 test('Getting the correct snake move (Backward, Odd Delta):') :- 
-  mksnake_backward(1, 3, 1, 0, [6,6,5,6,6,6], 6).
+  once(mksnake_backward(1, 3, 1, 0, [6,6,5,6,6,6], 6)).
 test('Getting the correct snake move (Backward, Odd Delta, Taking Smaller One and Move Up):') :- 
-  mksnake_backward(1, 3, 1, 1, [6,6,5,4,6,5], 4).
+  once(mksnake_backward(1, 3, 1, 1, [6,6,5,4,6,5], 4)).
 test('Getting the correct snake move (Backward, Odd Delta, Taking Smaller One and Move Left):') :- 
-  mksnake_backward(1, 3, 1, 1, [6,6,5,5,4,3], 2).
+  once(mksnake_backward(1, 3, 1, 1, [6,6,5,5,4,3], 2)).
 test('Getting the correct snake move (Backward, Odd Delta, Right Boundary):') :- 
-  mksnake_backward(1, 3, 2, 1, [6,6,5,5,4,6], 4).
+  once(mksnake_backward(1, 3, 2, 1, [6,6,5,5,4,6], 4)).
 test('Getting the correct snake move (Backward, Odd Delta, Left Boundary):') :- 
-  mksnake_backward(1, 3, 0, 2, [6,6,5,5,4,6], 3).
+  once(mksnake_backward(1, 3, 0, 2, [6,6,5,5,4,6], 3)).
 
 test('Check Finish Forward -- should finish') :- 
   check_finish_forward(5,5,3, 1, 4, 0, [0,1,2,3,4], 3).
 test('Check Finish Forward -- should not finish -- delta is even') :- 
-  not(check_finish_forward(6,5,3, 0, 4, 0, [0,1,2,3,4], X)).
+  not(check_finish_forward(6,5,3, 0, 4, 0, [0,1,2,3,4], _)).
 test('Check Finish Forward -- should not finish -- no match') :- 
   not(check_finish_forward(5,5,3, 1, 4, 0, [0,1,2,2,4], 3)).
 
 test('Check Finish Backward -- should finish') :- 
   check_finish_backward(3, 0, 4, 0, [0,1,2,3,4,5], 3).
 test('Check Finish Backward -- should not finish -- delta is odd') :- 
-  not(check_finish_backward(3, 1, D, K, [0,1,2,3,4,5], X)).
+  not(check_finish_backward(3, 1, _, _, [0,1,2,3,4,5], _)).
 test('Check Finish Backward -- should not finish -- no match') :- 
   not(check_finish_backward(3, 0, 4, 0, [0,1,2,2,2,2,2], 3)).
 
@@ -401,25 +399,25 @@ test('KLoop Simulation Backward: D=1 Finish') :-
   once(kloop_iter_backward(4, [f,a,b,b], [f,a,c,b], 0, 1, -1, 8, [3,3,3,3,2,3,3,3], [3,3,3,1,2,3,3,3], [0, 0, 0, 2, 1, 2, 0, 0], 2, 1)).
 
 test('DLoop Simulation: a .. a') :- 
-  once(dloop(2, [f,a], [f,a], 0, 0, 4, [0,0,0,0], [1,1,1,1], 1, 1, FOut, BOut)).
+  once(dloop(2, [f,a], [f,a], 0, 0, 4, [0,0,0,0], [1,1,1,1], 1, 1, _, _)).
 test('DLoop Simulation: aa .. aa') :- 
-  once(dloop(3, [f,a,a], [f,a,a], 0, 0, 6, [0,0,0,0,0,0], [2,2,2,2,2,2], 2, 2, FOut, BOut)).
+  once(dloop(3, [f,a,a], [f,a,a], 0, 0, 6, [0,0,0,0,0,0], [2,2,2,2,2,2], 2, 2, _, _)).
 test('DLoop Simulation: ab .. aa') :- 
-  once(dloop(3, [f,a,b], [f,a,a], 0, 0, 6, [0,0,0,0,0,0], [2,2,2,2,2,2], 1, 2, fout, bout)).
+  once(dloop(3, [f,a,b], [f,a,a], 0, 0, 6, [0,0,0,0,0,0], [2,2,2,2,2,2], 1, 2, _, _)).
 
 
 test(middle_snake_0) :-
-        middle_snake([], [], 0, 0).
+        once(middle_snake([], [], 0, 0)).
 test(middle_snake_k) :-
         middle_snake(['A'], ['A'], 1, 1).
 test(middle_snake_2) :-
         middle_snake(['A'], ['B'], 0, 1).
 test(middle_snake_3) :-
-        middle_snake(['A'], [], 1, 0). 
+        once(middle_snake(['A'], [], 1, 0)). 
 test(middle_snake_4) :-
         middle_snake(['A'], ['A','B'], 1, 2).
 test(middle_snake_5) :-
-        middle_snake(['A'], ['B','B'], 0, 2).
+        middle_snake(['A'], ['B','B'], 0, 1).
 test(middle_snake_6) :-
         middle_snake(['A','B','A'], ['A','B','A'], 3, 3). 
 test(middle_snake_7) :-
